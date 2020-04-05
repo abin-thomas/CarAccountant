15ba69f8fe2540daa967cc121fd817582ba046ea
abinthomas-Dev <abinthomas.dev@gmail.com>
2020-04-05 11:43:54 -0400

    Initial Structure

---
+++ app/src/main/java/com/example/caraccountant/expenseReport.java
@@ -0,0 +1,14 @@
+package com.example.caraccountant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class expenseReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_report);
+    }
+}

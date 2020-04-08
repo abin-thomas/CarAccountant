3e3b813cd45838148ca7914b76860a85e88862a7
abinthomas-Dev <abinthomas.dev@gmail.com>
2020-04-08 05:06:07 -0400

    Income page DONE

--- app/src/main/java/com/atmcorp/caraccountant/income.java
+++ app/src/main/java/com/atmcorp/caraccountant/income.java
@@ -3,26 +3,39 @@ package com.atmcorp.caraccountant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class income extends AppCompatActivity {
 
     /*******User Defined Variables**********/
+    SQLiteDatabase db;
+    String DB_NAME;
+
     DatePickerDialog datePicker;
     EditText et_date;
+    EditText et_startKms;
+    EditText et_endKms;
     EditText et_GrossIncome;
     EditText et_NoHstIncome;
     TextView tv_ApproxHST;
@@ -32,7 +45,12 @@ public class income extends AppCompatActivity {
     float tempNoHstEarning;
     float tempGrossEarning;
     float tempHstAmount;
+    float currYearNetIncome = 0;
+    float currYearHST = 0;
 
+    String stringDate;
+    int intStartKms;
+    int intEndKms;
     String stringEarningAmount;
     String stringHstAmount;
     String stringNetIncome;
@@ -47,27 +65,27 @@ public class income extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
 
+        fetchControls();
         showCalendarPopUp();
         earningsCalculation();
+        createIncomeChart();
     }
 
+    public void fetchControls()
+    {
+        et_date = findViewById(R.id.txt_date);
+        et_startKms = findViewById(R.id.txt_startKms);
+        et_endKms = findViewById(R.id.txt_endKms);
+        et_GrossIncome = findViewById(R.id.txt_totalIncome);
+        et_NoHstIncome = findViewById(R.id.txt_noHstIncome);
+        tv_ApproxHST = findViewById(R.id.txt_approxHst);
+        tv_NetIncome = findViewById(R.id.txt_netIncome);
 
-    public void clearAll(View view) {
-        ViewGroup group = (ViewGroup) findViewById(R.id.cLayoutMaster);
-        for (int i = 0, count = group.getChildCount(); i < count; ++i) {
-            View viewChild = group.getChildAt(i);
-            if (viewChild instanceof EditText) {
-                ((EditText)viewChild).setText("");
-            }
-
-            if(viewChild instanceof ViewGroup && (((ViewGroup)viewChild).getChildCount() > 0))
-                clearAll((ViewGroup)view);
-        }
     }
 
     public void showCalendarPopUp()
     {
-        et_date = findViewById(R.id.txt_date);
+
         et_date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,7 +117,6 @@ public class income extends AppCompatActivity {
 
     public void earningsCalculation()
     {
-        et_GrossIncome = findViewById(R.id.txt_totalIncome);
         et_GrossIncome.addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count)
@@ -112,11 +129,11 @@ public class income extends AppCompatActivity {
                 {
                     if(s.charAt(0)=='.')
                         et_GrossIncome.setText("0.");
-                    showResult();
+                    showNetIncome_HST();
                 }
                 else
                 {
-                    clearResult();
+                    clearNetIncome_HST();
                 }
             }
 
@@ -126,7 +143,6 @@ public class income extends AppCompatActivity {
 
 
         });
-        et_NoHstIncome = findViewById(R.id.txt_noHstIncome);
         et_NoHstIncome.addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count)
@@ -135,7 +151,7 @@ public class income extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s)
             {
-                showResult();
+                showNetIncome_HST();
             }
 
             @Override
@@ -146,10 +162,10 @@ public class income extends AppCompatActivity {
         });
     } //earningsCalculation
 
-    public void showResult()
+    public void showNetIncome_HST()
     {
 
-        et_NoHstIncome = findViewById(R.id.txt_noHstIncome);
+
         if(et_NoHstIncome.getText().length()!=0)
         {
             tempNoHstEarning = Float.parseFloat(et_NoHstIncome.getText().toString());
@@ -167,17 +183,109 @@ public class income extends AppCompatActivity {
         stringNetIncome = nf.format(tempGrossEarning - tempHstAmount);
 
 
-        tv_ApproxHST = findViewById(R.id.txt_approxHst);
         tv_ApproxHST.setText(stringHstAmount);
 
-
-        tv_NetIncome = findViewById(R.id.txt_netIncome);
         tv_NetIncome.setText(stringNetIncome);
     }
 
-    public void clearResult()
+    public void clearNetIncome_HST()
     {
         tv_ApproxHST.setText("");
         tv_NetIncome.setText("");
+    }
+
+    public void createIncomeChart()
+    {
+        fetchData(); //This function populates the 'currYearNetIncome' and 'currYearHST' from table
+
+        PieChartView pieChartView = findViewById(R.id.chart);
+        ArrayList<SliceValue> pieData = new ArrayList<SliceValue>();
+
+        pieData.add(new SliceValue(currYearNetIncome, Color.BLUE).setLabel("Net Income : "+currYearNetIncome));
+        pieData.add(new SliceValue(currYearHST, Color.GRAY).setLabel("HST : "+currYearHST));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasLabels(true);
+
+        pieChartView.setPieChartData(pieChartData);
+
+        pieChartView.setChartRotation(270,true);
+
+
+    }
+
+    private void fetchData()
+    {
+        DB_NAME = getString(R.string.DB_NAME); //getting the value from strings.xml
+        //create our db
+
+
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+
+        db = this.openOrCreateDatabase(DB_NAME, MODE_PRIVATE, null);
+
+        Cursor c = db.rawQuery("select " +
+                "sum(netEarning)," +
+                "sum(hst)" +
+                " from "+ getString(R.string.TABLE_INCOME) +
+                " where substr(dateField,7)like '"+ currentYear +"'",null);
+
+        if(c != null)
+        {
+            if(c.moveToFirst())
+            {
+                do{
+                    currYearNetIncome = c.getFloat(0);
+                    currYearHST = c.getFloat(1);
+                }while(c.moveToNext());
+
+            } //cursor if
+        } //null if
+
+        db.close();
+    }
+    public void fetchValues()
+    {
+        stringDate = et_date.getText().toString();
+
+        intStartKms =  Integer.parseInt(et_startKms.getText().toString());
+
+        intEndKms = Integer.parseInt(et_endKms.getText().toString());
+
+    }
+
+   public void callAddRecord(View view)
+    {
+        fetchValues(); // This is the function to read convert user values to the dbstyle data types.
+
+        DB_NAME = getString(R.string.DB_NAME);
+        db = this.openOrCreateDatabase(DB_NAME, MODE_PRIVATE, null);
+
+
+        ContentValues newValues = new ContentValues();
+        newValues.put("dateField", stringDate);
+        newValues.put("startKms", intStartKms);
+        newValues.put("endKms", intEndKms);
+        newValues.put("totalKms", (intEndKms - intStartKms));
+        newValues.put("eatsEarning",tempNoHstEarning);
+        newValues.put("grossEarning", stringEarningAmount);
+        newValues.put("hst", stringHstAmount);
+        newValues.put("netEarning", stringNetIncome);
+
+        //insert details
+        long result = db.insert(getString(R.string.TABLE_INCOME),null,newValues);
+
+        if(result != -1)
+            Toast.makeText(this, "New Record added ...", Toast.LENGTH_SHORT).show();
+
+        else
+            Toast.makeText(this, "Alert....Something wrong", Toast.LENGTH_SHORT).show();
+
+
+        db.close();
+
+        createIncomeChart();
+
     }
 } // class
